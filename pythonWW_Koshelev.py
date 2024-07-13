e92a447d1176ff2d9a1fab94c79bdcbb0c0ab885
Anton801106 <listboxx@yandex.ru>
2024-07-13 19:45:42 +0300

    Add files via upload

---
+++ pythonWW_Koshelev.py
@@ -0,0 +1,18 @@
+# Базовые структуры данных
+
+"1st program"
+print((int(9 ** 0.5) * 5))
+
+"2nd program"
+print(9.99 > 9.98 and 1000 != 100.1)
+
+"3rd  program"
+a = 1234
+b = 5678
+print((((a // 10) % 100)) + (((b // 10) % 100)))
+
+"4th  program"
+a = 13.42
+b = 42.13
+print(((int(a)) == int((b * 100) % 100) or int(a * 100) % 100))
+print(((int(b)) == int((a * 100) % 100) or int(b * 100) % 100))
